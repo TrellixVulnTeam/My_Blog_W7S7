12a7ec03c4d374f1a6550e06612d632c8eaaef95
Neeraj <ny.neeraj12121@gmail.com>
2017-01-11 00:42:58 +0530

    fixed missing url

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpattern = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
     url(r'^(P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'
         r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
 ]
