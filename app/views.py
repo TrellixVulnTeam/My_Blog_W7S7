53ce03ba1586be7e4995b597771d850e32cc9275
Neeraj <ny.neeraj12121@gmail.com>
2017-01-10 23:59:24 +0530

    changes in views

--- app/views.py
+++ app/views.py
@@ -1,2 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import structure
 
+
+def index(request):
+    posts = structure.published.all()
+    return render(request,'app/post/list.html',{'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    return render(request,'app/post/detail.html',{'post': post})
