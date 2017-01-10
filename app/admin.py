66c23bcbe15c151aaec1bdac3d4eb6bcb27ade5f
Neeraj <ny.neeraj12121@gmail.com>
2017-01-10 23:55:12 +0530

    updating admin pannel

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import structure
 
 
+class structureAdmin(admin.ModelAdmin):
+    list_display = {'title','published'}
+    list_filter = {'created','publish'}
+    search_fields = {'title', 'body'}
+    prepopulated_fields = {'slug':('title')}
+    raw_id_fields = 'author'
+    date_hierarchy = 'published'
+    ordering = ['published']
+
+
+
+
 admin.site.register(structure)
+
