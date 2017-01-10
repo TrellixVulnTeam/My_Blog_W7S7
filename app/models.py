91d2c6f48a799f07a4dab9befb9932a47d9c76ae
Neeraj <ny.neeraj12121@gmail.com>
2017-01-10 23:11:07 +0530

    setup venv

--- app/models.py
+++ app/models.py
@@ -1,14 +1,24 @@
-from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class structure(models.Model):
-    title=models.CharField(max_length=120)
+    title=models.CharField(max_length=250)
     content=models.TextField()
+    author=models.ForeignKey(User,related_name='blog_post')
+    created=models.DateTimeField(auto_now_add=True)
+    published = models.DateTimeField(default=timezone.now())
     updated=models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp=models.DateTimeField(auto_now=False, auto_now_add=True)
+    slug=models.SlugField(max_length=250,unique_for_date='published')
 
-    def __unicode__(self):
-        return self.title
+    class Meta:
+        ordering=("-published")
+
+
+
+
+
     def __str__(self):
         return self.title
