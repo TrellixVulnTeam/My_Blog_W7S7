50df8e7fde48c6695beaac17177e2e2ffd9f1158
Neeraj <ny.neeraj12121@gmail.com>
2017-01-11 00:27:53 +0530

    changes made in view

--- app/views.py
+++ app/views.py
@@ -4,8 +4,11 @@ from .models import structure
 
 def index(request):
     posts = structure.published.all()
-    return render(request,'app/post/list.html',{'posts': posts})
+    return render(request, 'list.html', {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-    return render(request,'app/post/detail.html',{'post': post})
+    post = get_object_or_404(structure, slug=post, status='published',
+                             publish_year=year, publish_month=month, publish_day=day)
+    return render(request, 'detail.html', {'post': post})
+
