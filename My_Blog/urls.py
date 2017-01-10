4d3ab1b0282ca5c70dbd32d4cb5f8bfc8f3dfa1c
Neeraj <ny.neeraj12121@gmail.com>
2017-01-11 00:27:27 +0530

    updated main urls

--- My_Blog/urls.py
+++ My_Blog/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^app/', include('app.urls', namespace='app',app_name='app'))
 ]
