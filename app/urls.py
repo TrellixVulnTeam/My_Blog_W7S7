c9101090cdf34abfb8844e7d0964363963f21c04
Neeraj <ny.neeraj12121@gmail.com>
2017-01-13 04:40:16 +0530

    changes in url patttern

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from . import views
+from app.views import (index,)
 
-
-urlpattern = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'
-        r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
+urlpatterns = [
+     url(r'^$', index),
+    # url(r'^(P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
 ]
