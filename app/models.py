1e002cc20916a30b58ab01508bb61f825046dc2c
Neeraj <ny.neeraj12121@gmail.com>
2017-01-13 04:10:06 +0530

    changes in database model

--- app/models.py
+++ app/models.py
@@ -1,19 +1,32 @@
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import now
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class structure(models.Model):
     title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250, unique_for_date='published', null=True)
     content=models.TextField()
-    author=models.ForeignKey(User,related_name='blog_post')
-    created=models.DateTimeField(auto_now_add=True)
-    published = models.DateTimeField(default=timezone.now())
+    author=models.ForeignKey(User,related_name='blog_post', null=True)
+    created=models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    published = models.DateTimeField(default=now, )
     updated=models.DateTimeField(auto_now=True, auto_now_add=False)
-    slug=models.SlugField(max_length=250,unique_for_date='published')
+    STATE_CHOICES = (
+        ('pending', 'Pending'),
+        ('published', 'Published')
+
+    )
+    status = models.CharField(max_length=15, choices=STATE_CHOICES, default='pending')
+
+
+  #  def get_absolute_url(self):
+   #     return reverse('app:post_detail',args=[self.published.year,
+    #                                           self.published.strftime('%m'),self.published.strftime('$d'),
+     #                                          self.slug])
 
     class Meta:
-        ordering=("-published")
+        ordering=('published', )
 
 
     def __str__(self):
