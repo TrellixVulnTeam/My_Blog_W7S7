61312e7901d1173c7a4866a78d59010235d16a37
Neeraj <ny.neeraj12121@gmail.com>
2017-01-13 04:10:47 +0530

    updated admin pannel

--- app/admin.py
+++ app/admin.py
@@ -3,13 +3,13 @@ from .models import structure, comment
 
 
 class structureAdmin(admin.ModelAdmin):
-    list_display = {'title','published'}
-    list_filter = {'created','publish'}
-    search_fields = {'title', 'body'}
-    prepopulated_fields = {'slug':('title')}
-    raw_id_fields = 'author'
+    list_display = ('title','published','status', )
+    list_filter = ('created','published', 'status', )
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    #raw_id_fields = ('author', )
     date_hierarchy = 'published'
-    ordering = ['published']
+    ordering = ('published', 'status')
 
 
 class commentAdmin(admin.ModelAdmin):
